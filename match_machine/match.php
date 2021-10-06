786ca30c9c58e3174505662222276521c55723ed
Samuel Ramirez <samuele.ramceb@educa.jcyl.es>
2021-10-06 20:51:18 +0200

    optimización del algoritmo, con funciones nativas de php

--- match_machine/match.php
+++ match_machine/match.php
@@ -1,36 +1,8 @@
 <?php
-/* 
-Guardar en dos arrays la dirección de las imagenes tanto de chicas como de chicos
-*/
-// Array Chicas
-$array_chicas = array(
-    "assets/chicas/chica_1.jpg",
-    "assets/chicas/chica_2.jpg",
-    "assets/chicas/chica_3.jpg",
-    "assets/chicas/chica_4.jpg",
-    "assets/chicas/chica_5.jpg"
-);
-// Array Chicos
-$array_chicos = array(
-    "assets/chicos/chico_1.jpg",
-    "assets/chicos/chico_2.jpg",
-    "assets/chicos/chico_3.jpg",
-    "assets/chicos/chico_4.jpg",
-    "assets/chicos/chico_5.jpg"
-);
 
-/* 
-Función Matching
-recibiendo como parametro dos cadenas con la ruta de las imagenes de tanto el chico como la chica (ya elegidas al azar)
-se encarga de cargarlas dentro de una etiqueta <img> para mostrarla en el navegador
- */
-function matching($chico, $chica)
+function matching()
 {
-    echo('<img src="'.$chico.'" width="350" height="480"> <img src="'.$chica.'" width="350" height="480">');
+    echo('<img src="assets/chicos/chico_'.rand(1,5).'.jpg" width="350" height="480"> <img src="assets/chicas/chica_'.rand(1,5).'.jpg" width="350" height="480">');
 }
 
-/* 
-Se utiliza la función 'array_rand()' que nos retorna una key al azar de un array dado, dicha clave se la pasamos
-al array original y las mismas a la funcion que se encargará de cargar dichas rutas en una etiqueta <img>
-*/
-matching($array_chicos[array_rand($array_chicos)], $array_chicas[array_rand($array_chicas)]);
+matching();
