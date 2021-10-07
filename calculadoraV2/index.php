b3b615e5156aae21ed2ab724425a209974d961a7
Samuel Ramirez <samuele.ramceb@educa.jcyl.es>
2021-10-07 20:28:53 +0200

    agregando especificaciones del ejercicio

--- calculadoraV2/index.php
+++ calculadoraV2/index.php
@@ -10,24 +10,24 @@
 </head>
 
 <body>
-    <form action="" method="get">
+    <form action="resultado.php" method="get">
         <table border="3">
             <tr>
                 <th colspan="5">CALCULADORA</th>
             </tr>
             <tr>
-                <td colspan="3"><label for="numero1">Operando 1:</label> <input type="number" name="numero1" id="numero1"></td>
-                <td colspan="2"><label for="numero2">Operando 2:</label> <input type="number" name="numero2" id="numero2"></td>
+                <td colspan="3"><label for="numero1">Operando 1:</label> <input type="number" name="OPERANDO1" id="numero1"></td>
+                <td colspan="2"><label for="numero2">Operando 2:</label> <input type="number" name="OPERANDO2" id="numero2"></td>
             </tr>
             <tr>
-                <td><input type="radio" name="operacion" id="1">suma</td>
-                <td><input type="radio" name="operacion" id="2">resta</td>
-                <td><input type="radio" name="operacion" id="3">division</td>
-                <td><input type="radio" name="operacion" id="4">producto</td>
-                <td><input type="radio" name="operacion" id="5">concatenacion</td>
+                <td><input type="radio" name="operacion" value="SUMA" id="1">suma</td>
+                <td><input type="radio" name="operacion" value="RESTA" id="2">resta</td>
+                <td><input type="radio" name="operacion" value="DIVISION" id="3">division</td>
+                <td><input type="radio" name="operacion" value="PRODUCTO" id="4">producto</td>
+                <td><input type="radio" name="operacion" value="CONCATENACION" id="5">concatenacion</td>
             </tr>
             <tr>
-                <td colspan="5"><input type="checkbox" name="negativos" id="neg">
+                <td colspan="5"><input type="checkbox" name="ROJOS" value="SI" id="neg">
                     <font class="checkBoxCalc">Mostrar valores negativos en rojo</font>
                 </td>
             </tr>
