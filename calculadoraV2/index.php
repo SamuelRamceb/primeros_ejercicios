2c0328e4e2a66df9a82324b679978b3ff616c9b7
Samuel Ramirez <samuele.ramceb@educa.jcyl.es>
2021-10-07 20:18:47 +0200

    Agregando estilos al proyecto calculadora V2

---
+++ calculadoraV2/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="" method="get">
+        <table border="3">
+            <tr>
+                <th colspan="5">CALCULADORA</th>
+            </tr>
+            <tr>
+                <td colspan="3"><label for="numero1">Operando 1:</label> <input type="number" name="numero1" id="numero1"></td>
+                <td colspan="2"><label for="numero2">Operando 2:</label> <input type="number" name="numero2" id="numero2"></td>
+            </tr>
+            <tr>
+                <td><input type="radio" name="operacion" id="1">suma</td>
+                <td><input type="radio" name="operacion" id="2">resta</td>
+                <td><input type="radio" name="operacion" id="3">division</td>
+                <td><input type="radio" name="operacion" id="4">producto</td>
+                <td><input type="radio" name="operacion" id="5">concatenacion</td>
+            </tr>
+            <tr>
+                <td colspan="5"><input type="checkbox" name="negativos" id="neg">
+                    <font class="checkBoxCalc">Mostrar valores negativos en rojo</font>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="5"><input type="button" value="Calcular"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+
+</html>
