5deb4d152de7d8419c4826e52782c32b895f9930
Samuel Ramirez <samuele.ramceb@educa.jcyl.es>
2021-10-08 19:24:17 +0200

    cambios menores

--- calculadoraV2/resultado.php
+++ calculadoraV2/resultado.php
@@ -8,40 +8,35 @@ $color = $_GET['ROJOS'];
 
 /* Llamado a la función de la calculadora */
 if (isset($operando1, $operando2, $operacion, $color)) {
-    /* llamar a la versión sin negativos */
+    calcula($operando1, $operando2, $operacion, $color);
+} else if (isset($operando1, $operando2, $operacion)) {
+    calcula($operando1, $operando2, $operacion);
 } else {
-    /* llamar a la versión negativos */
+    echo ("error en los datos!");
 }
-calcula($operando1, $operando2, $operacion);
 
-/* Funcionamiento de la calculadora */
-/* function calcula($n1, $n2, $op, $red) */
+
+function calcula($n1, $n2, $op, $red)
 {
-    
-    if(isset($n1, $n2, $op)){
-        
-        switch ($op) {
-            case 'SUMA':
-                echo (suma($n1, $n2));
-                break;
-            case 'RESTA':
-                echo (resta($n1,$n2));
-                break;
-            case 'PRODUCTO':
-                echo (multi($n1, $n2));
-                break;
-            case 'DIVISION':
-                echo (division($n1, $n2));
-                break;
-            case 'CONCATENACION':
-                echo(concatenacion($n1, $n2));
-                break;
-            default:
-                echo ("error");
-                break;
-        }
-    } else {
-        echo ("hubo un error en los datos");
+    switch ($op) {
+        case 'SUMA':
+            echo (suma($n1, $n2));
+            break;
+        case 'RESTA':
+            echo (resta($n1, $n2));
+            break;
+        case 'PRODUCTO':
+            echo (multi($n1, $n2));
+            break;
+        case 'DIVISION':
+            echo (division($n1, $n2));
+            break;
+        case 'CONCATENACION':
+            echo (concatenacion($n1, $n2));
+            break;
+        default:
+            echo ("error");
+            break;
     }
 }
 
