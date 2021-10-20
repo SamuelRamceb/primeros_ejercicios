28b6389c87bee8b0e3f33cff9c2ef919a2a36fd0
Samuel Ramirez <samuele.ramceb@educa.jcyl.es>
2021-10-20 19:16:13 +0200

    agregando la capacidad de subir imagenes

--- ficherosAlServer/php/mostrando.php
+++ ficherosAlServer/php/mostrando.php
@@ -1,10 +1,16 @@
 <?php
 
-$uploaddir = '../ficha/fotoserver/';
-$uploadfile = $uploaddir . basename($_FILES['userfile']['name']) . time();
+foreach ($_FILES as $key => $value) {
+    $uploaddir = '../ficha/fotoserver/';
+    $uploadfile = $uploaddir . basename($_FILES[$key]['name'], ".jpg") . time() . ".jpg";
 
-if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-    echo "File is valid, and was successfully uploaded.\n";
-} else {
-    echo "Possible file upload attack!\n";
+    if (move_uploaded_file($_FILES[$key]['tmp_name'], $uploadfile)) {
+        echo "File is valid, and was successfully uploaded." . "<br>";
+    } else if ($_FILES[$key]['size'] == 0) {
+        continue;
+    } else {
+        echo "Possible file upload attack!" . "<br>";
+    }
 }
+
+// var_dump($_FILES);
